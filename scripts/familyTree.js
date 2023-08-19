2a6493d2f14b8cbbcf7bfce0547db91ef484a9f1
Cextra <notmyemail.com>
2023-08-19 13:54:29 -0700

    Parent Tree Sort of Works

--- scripts/familyTree.js
+++ scripts/familyTree.js
@@ -3,7 +3,10 @@ var canvas = document.getElementById("familytree");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "black"; //text
 
-//users
+let thisUser = [
+    {"id": 7, "name": "Cextra"}
+]
+
 let individualTable = [
     { "id": 1, "name": "Alice", "birthdate": "1990-01-01" },
     { "id": 2, "name": "Bob", "birthdate": "1995-05-15" },
@@ -37,6 +40,8 @@ let marriages = [
     { "marriage_id": 5, "spouse1_id": 15, "spouse2_id": 19 }
 ];
 
+
+
 //showing heiarchy
   let hierarchicalRelationships = [
     { "ancestor_id": 1, "descendant_id": 2, "depth": 1 },
@@ -59,42 +64,112 @@ let marriages = [
 
   //for children
   let childParentRelatinoshipsTable = [
-    { "child_id": 3, "parent1_id": 1, "parent2_id": 2 },
-    { "child_id": 5, "parent1_id": 1, "parent2_id": 2 },
-    { "child_id": 7, "parent1_id": 2, "parent2_id": 1 },
-    { "child_id": 9, "parent1_id": 2, "parent2_id": 3 }
+    { "child_id": 3, "parent1_id": 30, "parent2_id": 2 },
+    { "child_id": 1, "parent1_id": 6, "parent2_id": 9 },
+    { "child_id": 5, "parent1_id": 6, "parent2_id": 9 },
+    { "child_id": 7, "parent1_id": 3, "parent2_id": 1 },
+    { "child_id": 2, "parent1_id": 6, "parent2_id": 9 },
+    { "child_id": 6, "parent1_id": 12, "parent2_id": 15 },
+    { "child_id": 12, "parent1_id": 120, "parent2_id": 30 },
+    { "child_id": 30, "parent1_id": 1210, "parent2_id": 99 },
+    { "child_id": 15    , "parent1_id": 69, "parent2_id": -2 }
   ]
   
-  
-  function drawFamilyTree(x, y, individualId, generation) {
-    const individual = individualTable.find(individual => individual.id === individualId);
-    ctx.fillText(individual.name, x, y);
-  
-    const children = childParentRelatinoshipsTable.filter(rel => rel.parent1_id === individualId || rel.parent2_id === individualId);
-    console.log(children);
-    if (children.length > 0) {
-      y += 40;
-  
-      for (const child of children) {
+  //keeps track of drawing cursor
+  let cursorX = 0;
+  let cursorY = 0;
+
+  function drawParent(x, y, id, depth){
+
+    thisID = id; //_currentUserID
+    const parentList = childParentRelatinoshipsTable.filter( (col) => col.child_id === thisID); //parents
+
+
+    //get some calcs
+    generation_depth = 0;
+    calculateGeneration(thisID);
+
+    if(generation_depth == 0){
+        setFillStyle("red");
+    } else {
+        setFillStyle("white");
+    }
+
+    const distance = 60 + 20 * depth;
+
+    //draw
+    ctx.fillText("parent"+id, x, y); //draw parent
+
+    //left line
+    if(generation_depth > 0){
+    ctx.beginPath();
+    ctx.moveTo(x - (distance / 2), y - 40);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    }
+
+    //right line
+    if(generation_depth > 0){
         ctx.beginPath();
-        ctx.moveTo(x, y + 20);
+        ctx.moveTo(x + (distance / 2), y - 40);
         ctx.lineTo(x, y);
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 2;
         ctx.stroke();
-        drawFamilyTree(x, y, child.child_id, generation + 1);
-        y += 40;
-      }
+    }
+
+    console.log("I am parent"+thisID + " I have a generation_depth of " + generation_depth + ": This should be my distance - " + distance);
+
+    //iterate
+    for(const key in parentList){
+        drawParent(x - (distance / 2), y - 40, parentList[key].parent1_id, generation_depth);
+        drawParent(x + (distance / 2), y - 40, parentList[key].parent2_id, generation_depth);
+    }
+
+  }
+
+  function setFillStyle(color){
+    ctx.fillStyle = color;
+  }
+  
+  function startDrawingParentTree(x = canvas.width / 2, y = canvas.height / 2) {
+    ctx.strokeStyle='black';
+    cursorX = x;
+    cursorY = y;
+
+    const rootID = thisUser[0].id;
+
+    const parentList = childParentRelatinoshipsTable.filter( (col) => col.child_id === rootID);
+
+    generation_depth = 0;
+    calculateGeneration(rootID); //initial calculation to calculate {generation_depth} for starting width
+
+    const distance = 70 * generation_depth;
+
+    drawParent(cursorX - distance / 2, cursorY - 5, parentList[0].parent1_id, generation_depth);
+    drawParent(cursorX + distance / 2, cursorY - 5, parentList[0].parent2_id, generation_depth);
+
+  }
+
+
+  //generation_depth knows how many generations the family tree goes back
+  //this is used to set the initial starting width of the tree
+  let generation_depth = 0;
+  function calculateGeneration(rootID, parent_depth = 0){
+
+    const parents = childParentRelatinoshipsTable.filter( (item) => item.child_id == rootID) //returns a table with parents
+
+    if(parent_depth > generation_depth){
+        generation_depth = parent_depth;
+    }
+
+    //has parents
+    if(parents[0] != undefined){
+        calculateGeneration(parents[0].parent1_id, parent_depth + 1);
+        calculateGeneration(parents[0].parent2_id, parent_depth + 1);
     }
   }
   
-  function startDrawingFamilyTree(x = 30, y = 30) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    const rootIndividuals = individualTable.filter(individual => !childParentRelatinoshipsTable.some(rel => rel.child_id === individual.id));
-  
-    for (const root of rootIndividuals) {
-      drawFamilyTree(x, y, root.id, 0);
-      x += 40; // Offset for multiple root individuals
-    }
-  }
-  
-  startDrawingFamilyTree();
+  startDrawingParentTree();
